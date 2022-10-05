256fb59dbac64eb5166bc421d038003d94b62103
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-10-05 17:13:18 +0300

    styled components

---
+++ utils/fetchAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com"
+
+
+  export const fetchApi = async (url) => {
+    const { data } = await axios.get((url),{
+        headers: {
+            'X-RapidAPI-Key': '1093047bc9msh1375ae17a2a753cp165fdejsnd28fe78f4e18',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+          }
+
+    })
+    return data;
+  } 
