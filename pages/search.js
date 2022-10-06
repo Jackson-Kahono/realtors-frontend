5f676a30505819ab8c23ef07ac3a2d3377426e99
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-10-06 13:07:23 +0300

    styled search page and added onclick button

--- pages/search.js
+++ pages/search.js
@@ -3,3 +3,31 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
+
+const Search = () => {
+    const [searchFilters, setSearchFilters] = useState(false);
+    const router = useRouter();
+
+    return (
+      <Box>
+        <Flex
+        cursor="pointer"
+        bg="gray.200"
+        borderBottom="2px"
+        borderColor="gray.400"
+        padding="2"
+        fontWeight="black"
+        fontSize="large"
+        justifyContent="center"
+        alignItems="center"
+        onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
+        >
+            <Text>Search properties by filter</Text>
+            <Icon paddingLeft="2" width="7" as={BsFilter} />
+        </Flex>
+      </Box>  
+
+
+    )
+}
+export default Search;
