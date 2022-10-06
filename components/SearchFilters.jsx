f666d675a6005b3695b4c61128c776285913a046
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-10-06 16:26:39 +0300

    render searchFilter items

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,12 +3,30 @@ import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-u
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-    const [filters, setFilters] = useState({});
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filterValues) => {
+
+    }
 
 return (
     <Flex background="gray.200" padding="4" justifyContent="center" flexWrap="wrap">
+        {filters.map((filter) => (
+            <Box key={filter.queryName}>
+                <Select 
+                placeholder={filter.placeholder}
+                width="fit-content"
+                padding="2"
+                onChange={(event) => searchProperties({[ filter.queryName ] : event.target.value})}>
+
+                </Select>
+
+            </Box>
+        )
+        )}
 
     </Flex>
 )
